bfe9d24a2a2561630829191618e2f1198b23b7a9
Ricky Setiawan <ricky.rickysetiawan.setiawan@gmail.com>
2021-01-25 21:29:05 +0700

    test

---
+++ src/test/java/com/ricky/example/AppTest.java
@@ -0,0 +1,27 @@
+package com.ricky.example;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+import org.testng.asserts.*;
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+
+    @Test
+    public void shouldAnswerWithFalse()
+    {
+        assertTrue(false );
+    }
+
+}
